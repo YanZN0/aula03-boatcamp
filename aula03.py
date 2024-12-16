5e8f628d07271452f3ab504525e4d08b9a8b17c0
YanZN <yanrobim@gmail.com>
2024-12-16 16:42:47 -0300

    minha aula 03 do boatcamp

---
+++ aula03.py
@@ -0,0 +1,22 @@
+# quantidade = input("Digite sua quantidade")
+# valor = input("Digite seu valor")
+
+# if quantidade > "0" and valor > "0":
+#     print("Valor e quantidade valídos")
+# else:
+#     print("Valores e quantidades invalidos")
+
+# alunos_presença = ["Aluno1", "Aluno2", "Aluno3"]
+
+# for alunos in alunos_presença:
+#     print(alunos)
+
+
+# dados = []
+# entrada = ""
+# while entrada.lower() != "sair":
+#     entrada = input("Digite um valor (ou 'sair' para terminar): ")
+#     if entrada.lower() != "sair":
+#         print("Você quer continuar, continue:")
+#     else:
+#         print("Sua decisão foi sair...")
